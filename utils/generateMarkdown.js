81e176cc376458d377f42fd378f3fee9084bbfc5
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-08-29 22:57:01 -0700

    Cleans code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {  
   if (license === "Academic Free License v3.0") {
     return`![Static Badge](https://img.shields.io/badge/License-AFL_3.0-lightblue.svg)`
@@ -78,8 +76,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "Academic Free License v3.0") {
     return `[https://opensource.org/license/afl-3-0-php](https://opensource.org/license/afl-3-0-php)`
@@ -158,8 +154,6 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   return `
 <p> This repo is under ${license} license.  Please see link below for more details. 
@@ -169,7 +163,6 @@ ${renderLicenseLink(license)}
   `
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
   return `
 # ${response.title}
